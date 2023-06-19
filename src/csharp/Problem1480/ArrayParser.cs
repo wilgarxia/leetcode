de17bddb7d472176b40b45fc94923e22f327651c
wilgarxia <wilgrxia@gmail.com>
2023-06-19 17:46:56 -0300

    feat: added 1480 for c#

---
+++ src/csharp/Problem1480/ArrayParser.cs
@@ -0,0 +1,21 @@
+﻿namespace Problem1480;
+
+public class ArrayParser
+{
+    public static int[] ParseArray(string[] input)
+    {
+        int[] result = new int[input.Length];
+
+        for (int i = 0; i < input.Length; i++)
+        {
+            if (!int.TryParse(input[i], out int parsedValue))
+            {
+                return Array.Empty<int>();
+            }
+            
+            result[i] = parsedValue;
+        }
+
+        return result;
+    }
+}
---
+++ src/csharp/Problem1480/Solution.cs
@@ -0,0 +1,28 @@
+﻿namespace Problem1480;
+
+public class Solution
+{
+    public int[] RunningSum(int[] nums)
+    {
+        for (int i = 1; i < nums.Length; i++)
+        {
+            nums[i] += nums[i - 1];
+        }
+
+        return nums;
+    }
+
+    // This version defines a new array instead of using the original array.
+    public int[] RunningSumAlternative(int[] nums)
+    {
+        int[] results = new int[nums.Length];
+        results[0] = nums[0];
+
+        for (int i = 1; i < nums.Length; i++)
+        {
+            results[i] = nums[i] + results[i - 1];
+        }
+
+        return results;
+    }
+}
