707809dded9e7a6f495a283177e4f7193cf03aea
wilgarxia <wilgrxia@gmail.com>
2023-06-20 00:03:35 -0300

    chore: better folder structure

--- src/1480/csharp/RunningSumOf1dArray/Program.cs
+++ src/1480/csharp/RunningSumOf1dArray/Program.cs
@@ -22,17 +22,4 @@ public class Solution
 
         return nums;
     }
-
-    public static int[] RunningSumAlternative(int[] nums)
-    {
-        int[] results = new int[nums.Length];
-        results[0] = nums[0];
-
-        for (int i = 1; i < nums.Length; i++)
-        {
-            results[i] = nums[i] + results[i - 1];
-        }
-
-        return results;
-    }
 }
