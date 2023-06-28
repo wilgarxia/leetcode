6b3b4c1bbf8f68bd434f35bec41d5455424d6ae5
wilgarxia <wilgrxia@gmail.com>
2023-06-28 07:07:50 -0300

    chore: added comment to explain bitwise shift operation and bitmask

--- src/1342/csharp/NumberOfStepsToReduceANumberToZero/Program.cs
+++ src/1342/csharp/NumberOfStepsToReduceANumberToZero/Program.cs
@@ -19,6 +19,13 @@ public class Solution
 
         for (int i = num; i > 0;)
         {
+            /*
+            1 here is a bitmask = 00000001. 
+            & is a bitwise logical operation.
+            Even numbers have their last bit equal to 0.
+            When a number is even, the result will be 1. Then the number will be subtracted in the else block.
+            When a number is odd, the result will be 0 (0 AND 1). Then the number will be halved in the if block.
+            */
             if ((i & 1) == 0)
                 i >>= 1;
             else
